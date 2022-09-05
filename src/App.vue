fc4205dbba810ee352b9bbe4f8919a8dac888c88
ouarrho oualid <34518781+ouarrho@users.noreply.github.com>
2022-09-05 18:34:11 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+
+	<div class="container p-4">
+
+		<h1>Hello :)</h1>
+
+	</div>  
+
+</template>
+
+<script>
+
+	export default {
+
+		name: 'App',
+
+		components: {
+
+			
+
+		}
+
+	}
+
+</script>
+
+<style>
+
+
+</style>
