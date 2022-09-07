d4e6d18912cbb4cb8d70340c5659ddbd79f839bb
ouarrho oualid <34518781+ouarrho@users.noreply.github.com>
2022-09-07 18:18:27 +0200

    Create About.vue

---
+++ src/components/About.vue
@@ -0,0 +1,27 @@
+<template>
+
+	<div>About</div>
+
+</template>
+
+<script>
+
+	export default {
+
+		name: 'app-about',
+
+		props: {
+
+			msg: String
+
+		}
+
+	}
+
+</script>
+
+<style scoped>
+
+
+
+</style>
